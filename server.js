15bd0c34896158103b0a2b6772b1f0cf2a53c7ea
julymisuzu <juhsfm@gmail.com>
2018-04-15 23:46:07 -0300

    NodeJS - Changing the content based on the url

--- server.js
+++ server.js
@@ -5,12 +5,22 @@ var fs = require('fs');
 
 
 http.createServer(function(request, response) {
+  /** Reading a file a printing it's whole content **/
+  /** To use this, comment everthing else **/
+  var newUrl = url.parse(request.url, true);
+  var filename = "." + newUrl.pathname;
 
-  fs.rename('test.html', 'testing2.html', function(error) {
-    if(error) throw error;
-    console.log('File Renamed!');
-  })
+  fs.readFile(filename, function(err, data) {
+    if(err) {
+      response.writeHead(404, {'Content-Type': 'text/html'});
+      return response.end('404 Not Found');
+    }
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(data);
+    return response.end();
+  });
+  /****/
 
-  response.end();
+  // response.end();
 
 }).listen(8080);
