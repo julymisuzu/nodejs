49f736cdb59b80dfe8765fac700e801e185b3381
julymisuzu <juhsfm@gmail.com>
2018-04-15 23:43:50 -0300

    NodeJS - Reading an external file

--- server.js
+++ server.js
@@ -1,30 +1,14 @@
 var http = require('http');
 var testingData = require('./testingData.js');
 var url = require('url');
+var fs = require('fs');
 
 
 http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
 
-  response.write('<h1>Hello World!</h1>');
-  response.write('<br>'+'Data: '+testingData.myDateTime());
-  response.write('<br>'+request.url);
-
-  var q = url.parse('http://localhost:8080/default.htm?year=2017&month=february', true);
-  var txt = q.query.year + " " + q.query.month;
-  response.write('<br>'+txt);
-
-  /** Writing NodeJS url.parse info **/
-  var nodeKeys = Object.keys(q);
-  var nodeValues = Object.values(q);
-
-  console.log('Node length: '+nodeKeys.length);
-  for(var i = 0; i < nodeKeys.length; i++) {
-    var keyValue = nodeKeys[i];
-    console.log(nodeKeys[i],':',nodeValues[i]);
-  }
-  /****/
-
-  response.end();
+  fs.readFile('test.html', function(err, data) {
+    response.write(data);
+    response.end();
+  });
 
 }).listen(8080);
