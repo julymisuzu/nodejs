6a59d6a0429f8ebb1e38027ea7d268d59335271d
julymisuzu <juhsfm@gmail.com>
2018-04-15 23:47:29 -0300

    NodeJS - Testing Node events

--- server.js
+++ server.js
@@ -2,25 +2,10 @@ var http = require('http');
 var testingData = require('./testingData.js');
 var url = require('url');
 var fs = require('fs');
+var manipulatingEvents = require('./manipulatingEvents.js');
 
 
 http.createServer(function(request, response) {
-  /** Reading a file a printing it's whole content **/
-  /** To use this, comment everthing else **/
-  var newUrl = url.parse(request.url, true);
-  var filename = "." + newUrl.pathname;
-
-  fs.readFile(filename, function(err, data) {
-    if(err) {
-      response.writeHead(404, {'Content-Type': 'text/html'});
-      return response.end('404 Not Found');
-    }
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write(data);
-    return response.end();
-  });
-  /****/
-
-  // response.end();
-
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.end();
 }).listen(8080);
