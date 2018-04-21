253c0c90edc47547b48f5ae04149d9a324e47746
julymisuzu <juhsfm@gmail.com>
2018-04-21 10:34:47 -0300

    Express - Using template

--- server.js
+++ server.js
@@ -1,26 +1,72 @@
 /*** Core ***/
 var express = require('express');
 var app = express();
-var router = express.Router();
+var bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+/*** Imports ***/
+var birds = require('./birds');
+var myMiddleware = require('./myMiddleware');
 
 /*** Server ***/
 app.listen(8080, function() {
   console.log('App listening on port 8080!');
 });
 
+/*** Using template ***/
+app.set('view engine', 'pug');
+app.set('views', './views');
 
-/*** Middleware with Router ***/
-router.use(function(request, response, next) {
-  console.log('Middleware Router Time: ', Date.now());
+app.get('/pugView', function(request, response) {
+  response.render('index', { title: 'Hey Pug', message: 'Hello there!' });
+});
+
+
+/*** Middlware Requests ***/
+var requestTime = function(request, response, next) {
+  console.log('Middleware requestTime');
+  request.requestTime = Date.now();
   next();
-});
-app.use('/cat', function(request, response, next) {
-  console.log('app.use(/cat)');
-  next('route');
-});
-router.get('/createCat', function(request, response) {
-  console.log('router.get(/createCat)');
+}
+app.use(requestTime);
+
+app.use(myMiddleware({option1: "firstOption", option2: "secondOption"}));
+
+
+/*** Requests ***/
+app.get('/', function(request, response) {
+  console.log('app.get(/)');
+  response.send('Hello World! '+request.requestTime);
 });
 
-// This set the url to load the router
-app.use('/cat', router);
+app.use('/user/create', function(request, response) {
+  console.log('app.use(/user/create)');
+  response.send('You are creating a user');
+});
+
+app.put('/user', function(request, response) {
+  console.log('app.put(/user)');
+  response.send('You are in the user page');
+});
+
+app.get('/user/:userId/book/:bookId', function(request, response) {
+  console.log('app.get(/user/:userId/book/:bookId)');
+  response.send(request.params);
+});
+
+app.get('/flights/:fromId-:toId', function(request, response) {
+  console.log('app.get(/flights/:fromId-:toId)');
+  response.send('Your are going from: '+request.params.fromId+' to '+request.params.toId);
+});
+
+app.use('/birds', birds);
+app.use('/birds/about', birds);
+
+// This MUST be the last command
+app.use(function(request, response, next) {
+  response.status(404).send('Not found!');
+});
