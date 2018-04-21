324899b40e2523796cc8b40474429266ac700272
julymisuzu <juhsfm@gmail.com>
2018-04-21 10:24:00 -0300

    Express - Router-Level Middleware

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 /*** Core ***/
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
-app.use(bodyParser.json());
-
-/*** Imports ***/
-var birds = require('./birds');
-var myMiddleware = require('./myMiddleware');
+var router = express.Router();
 
 /*** Server ***/
 app.listen(8080, function() {
@@ -18,47 +9,18 @@ app.listen(8080, function() {
 });
 
 
-/*** Middlware Requests ***/
-var requestTime = function(request, response, next) {
-  console.log('Middleware requestTime');
-  request.requestTime = Date.now();
+/*** Middleware with Router ***/
+router.use(function(request, response, next) {
+  console.log('Middleware Router Time: ', Date.now());
   next();
-}
-app.use(requestTime);
-
-app.use(myMiddleware({option1: "firstOption", option2: "secondOption"}));
-
-
-/*** Requests ***/
-app.get('/', function(request, response) {
-  console.log('app.get(/)');
-  response.send('Hello World! '+request.requestTime);
+});
+app.use('/cat', function(request, response, next) {
+  console.log('app.use(/cat)');
+  next('route');
+});
+router.get('/createCat', function(request, response) {
+  console.log('router.get(/createCat)');
 });
 
-app.use('/user/create', function(request, response) {
-  console.log('app.use(/user/create)');
-  response.send('You are creating a user');
-});
-
-app.put('/user', function(request, response) {
-  console.log('app.put(/user)');
-  response.send('You are in the user page');
-});
-
-app.get('/user/:userId/book/:bookId', function(request, response) {
-  console.log('app.get(/user/:userId/book/:bookId)');
-  response.send(request.params);
-});
-
-app.get('/flights/:fromId-:toId', function(request, response) {
-  console.log('app.get(/flights/:fromId-:toId)');
-  response.send('Your are going from: '+request.params.fromId+' to '+request.params.toId);
-});
-
-app.use('/birds', birds);
-app.use('/birds/about', birds);
-
-// This MUST be the last command
-app.use(function(request, response, next) {
-  response.status(404).send('Not found!');
-});
+// This set the url to load the router
+app.use('/cat', router);
