f433f279cce348387af8e6c144a5569ce249e689
julymisuzu <juhsfm@gmail.com>
2018-04-23 00:43:29 -0300

    bookApp - Insert, Edit and Remove working. Error handler done.

--- javascript/main.js
+++ javascript/main.js
@@ -2,89 +2,64 @@ $(function() {
   // load all the books results
   getBooksList();
 
+  function successCallout(message) {
+    $('#successCallout').fadeIn(400);
+    $('#successMessage').html(message);
+
+    setTimeout(function() {
+      $('#successCallout').fadeOut(400);
+    }, 2000);
+  }
+
+  function errorCallout(message) {
+    $('#errorCallout').fadeIn(400);
+    $('#errorMessage').html(message);
+
+    setTimeout(function() {
+      $('#errorCallout').fadeOut(400);
+    }, 2000);
+  }
+
   // when a new book is created
-  $('form').on('submit', function(formData) {
+  $('#createForm').on('submit', function(formData) {
     formData.preventDefault();
 
-    var bookName = formData.target[0].value;
-    var authorName = formData.target[1].value;
+    var formType = formData.target[0].value;
+    var bookId = formData.target[1].value;
+    var bookName = formData.target[2].value;
+    var authorName = formData.target[3].value;
 
     if(bookName !== "" && authorName !== "") {
-      var books = { book: bookName, author: authorName };
-      $.post('/books/create', books).success(getBooksList);
+      var books = { id: bookId, book: bookName, author: authorName };
+      if(formType === "edit" && bookId) {
+        $.post('/books/edit', books).done(function() {
+          $('#bookId, #formType, #bookName, #authorName').val('');
+          $('#cancelEdit').hide();
+          $('#saveEdit').hide();
+          $('#submitForm').show();
+          getBooksList();
+          successCallout('The book has been edited');
+        }).fail(function(xhr, textStatus, errorThrown) {
+          errorCallout(xhr.responseText);
+        });
+      } else {
+        $.post('/books/create', books).done(function() {
+          $('#bookId, #formType, #bookName, #authorName').val('');
+          getBooksList();
+          successCallout('The book has been created!');
+        }).fail(function(xhr, textStatus, errorThrown) {
+          errorCallout(xhr.responseText);
+        });
+      }
     }
   });
 
-  /*$('#orderByAuthor').on('click', function() {
-    $.get('/books', function(data) {
-      var li = {}, h4, p;
-      var dataJSON = JSON.parse(data);
-
-      var authorList = {};
-
-      // debugger;
-
-      var test = new Array();
-      dataJSON.livros.forEach(function(item, key) {
-        console.log(item.author);
-        console.log(test[item.author]);
-        // debugger;
-        var list = test[item.author];
-    
-        if(list) {
-          list.push(item.name);
-        } else {
-          test[item.author] = new Array(item.name);
-
-          li[test.indexOf(item.author)] = document.createElement("li");
-
-          h4 = document.createElement("h4");
-          h4.appendChild(document.createTextNode(item.author));
-          li[test.indexOf(item.author)].appendChild(h4);
-        }
-
-        span = document.createElement("span");
-        span.appendChild(document.createTextNode(item.name));
-        li[test.indexOf(item.author)].appendChild(span);
-      });
-      
-      console.log(test, li);
-
-      // dataJSON.livros.forEach(function(item, key) {
-      //   var list = authorList[item.author];
-
-      //   if(list) {
-      //     list.push(item.name);
-      //   } else {
-          
-      //     authorList[item.author] = [item.name];
-      //   }
-
-      //   // p.appendChild(document.createTextNode(item.name));
-      //   // li[key].appendChild(p);
-      // });
-
-      // console.log(authorList);
-      // console.log(li);
-
-      // debugger;
-
-      // dataJSON.livros.forEach(function(item, key) {
-      //   // create a dynamic list item
-      //   li[key] = document.createElement("li");
-      //   h4 = document.createElement("h4");
-      //   span = document.createElement("span");
-      //   // set a value
-      //   h4.appendChild(document.createTextNode(item.name));
-      //   span.appendChild(document.createTextNode(item.author));
-      //   li[key].appendChild(h4);
-      //   li[key].appendChild(span);
-
-      // });
-
-      // $('#results').html(li);
-    });
-  });*/
+  $('#cancelEdit').on('click', function() {
+    $('#bookId, #formType, #bookName, #authorName').val('');
+    $('#cancelEdit').hide();
+    $('#saveEdit').hide();
+    $('#submitForm').show();
+  });
 
   function getBooksList() {
     $.get('/books', function(data) {
@@ -92,30 +67,75 @@ $(function() {
       var dataJSON = JSON.parse(data);
 
       dataJSON.livros.forEach(function(item, key) {
-        // debugger;
         // create a dynamic list item
         li[key] = $("<li />", { class: "grid-x grid-padding-x" })[0];
         h4 = $("<h4 />", { text: item.name, class: "medium-12 cell" })[0];
         p = $("<p />", { text: item.author, class: "medium-12 cell" })[0];
-        edit = $("<a />", { text: "Edit", class: "medium-2 cell" });
+
+        editForm = $('<form />', { name: 'editForm-'+key, 
+                                   class: 'editForm', 
+                                   action: '/books/edit', 
+                                   method: 'post' });
+        editFormBook = $('<input />', { type: 'text', name: 'editBookName', value: item.name })[0];
+        editFormAuthor = $('<input />', { type: 'text', name: 'editAuthorName', value: item.author })[0];
+        editFormSave = $('<input />', { type: 'submit', value: 'Save', class: 'button' })[0];
+
+        edit = $("<a />", { text: "Edit", 
+                            class: "medium-2 cell", 
+                            "data-id": key, 
+                            "data-book": item.name, 
+                            "data-author": item.author });
         edit.on('click', function(key) {
-          console.log(key);
+          var id = event.currentTarget.dataset.id;
+          var book = event.currentTarget.dataset.book;
+          var author = event.currentTarget.dataset.author;
+
+          $('#bookId').val(id);
+          $('#formType').val("edit");
+          $('#bookName').val(book);
+          $('#authorName').val(author);
+          $('#cancelEdit').show();
+          $('#saveEdit').show();
+          $('#submitForm').hide();
         });
+
         remove = $("<a />", { text: "Remove", "data-id": item.id, class: "medium-3 cell" });
         remove.on('click', function(event) {
           var id = event.currentTarget.dataset.id;
 
-          $.post('/books/remove', {bookId: id}).success(getBooksList);
+          $.post('/books/remove', {bookId: id}).success(function() {
+            getBooksList();
+            successCallout('The book has been removed from the list');
+          });
         });
 
+        editForm.on('submit', function(formData) {
+          formData.preventDefault();
+      
+          var bookName = formData.target[0].value;
+          var authorName = formData.target[1].value;
+
+          if(bookName !== "" && authorName !== "") {
+            var books = { book: bookName, author: authorName };
+            $.post('/books/edit', books).success(getBooksList);
+          }
+        });
+
+        $(editForm).append(editFormBook)
+                   .append(editFormAuthor)
+                   .append(editFormSave)
+                   .hide();
         $(li[key]).append(h4)
                   .append(p)
+                  .append(editForm)
                   .append(edit)
                   .append(remove)
                   .append($("<hr />", { class: "medium-12" })[0]);
       });
 
       $('#results').html(li);
+    }).fail(function(xhr, textStatus, errorThrown) {
+      errorCallout(xhr.responseText);
     });
   }
 
--- server.js
+++ server.js
@@ -33,21 +33,61 @@ app.get('/', function(request, response) {
 
 app.get('/books', function(request, response) {
   fs.readFile(jsonUrl, function(error, data) {
-    if(error) return response.send('No books found.');
+    if(error) {
+      console.log('[BOOKS] Error - '+error);
+      response.writeHead(404, {'Content-Type': 'text/html'});
+      return response.end('An inexpected error occurred.');
+    }
     response.send(data);
   });
 });
 
 app.post('/books/create', function(request, response) {
   fs.readFile(jsonUrl, function(error, data) {
-    if(error) return response.send('No books found.');
+    if(error) {
+      console.log('[BOOKS CREATE READ] Error - '+error);
+      response.writeHead(404, {'Content-Type': 'text/html'});
+      return response.end('An inexpected error occurred.');
+    }
+
+    var booksList = JSON.parse(data);
+    var newBookId = booksList.livros[booksList.livros.length - 1].id + 1;
+
+    booksList.livros.push({ id: newBookId, name: request.body.book, author: request.body.author });
+
+    fs.writeFile(jsonUrl, JSON.stringify(booksList), function(error) {
+      if(error) {
+        console.log('[BOOKS CREATE WRITE] Error - '+error);
+        response.writeHead(404, {'Content-Type': 'text/html'});
+        return response.end('An inexpected error occurred.');
+      }
+      console.log('The books list has been updated');
+      response.end();
+    });
+  });
+});
+
+app.post('/books/edit', function(request, response) {
+  fs.readFile(jsonUrl, function(error, data) {
+    if(error) {
+      console.log('[BOOKS EDIT READ] Error - '+error);
+      response.writeHead(404, {'Content-Type': 'text/html'});
+      return response.end('An inexpected error occurred.');
+    }
+
     var booksList = JSON.parse(data);
 
-    booksList.livros.push({ name: request.body.book, author: request.body.author });
+    booksList.livros[request.body.id].name = request.body.book;
+    booksList.livros[request.body.id].author = request.body.author;
 
     console.log(booksList);
     fs.writeFile(jsonUrl, JSON.stringify(booksList), function(error) {
-      if(error) return response.send('Not writing possibility');
+      if(error) {
+        console.log('[BOOKS EDIT WRITE] Error - '+error);
+        response.writeHead(404, {'Content-Type': 'text/html'});
+        return response.end('An inexpected error occurred.');
+      }
+
       console.log('The books list has been updated');
       response.end();
     });
@@ -56,9 +96,11 @@ app.post('/books/create', function(request, response) {
 
 app.post('/books/remove', function(request, response) {
   fs.readFile(jsonUrl, function(error, data) {
-    if(error) return response.send('No books found.');
-
-    
+    if(error) {
+      console.log('[BOOKS REMOVE READ] Error - '+error);
+      response.writeHead(404, {'Content-Type': 'text/html'});
+      return response.end('An inexpected error occurred.');
+    }
 
     var booksList = JSON.parse(data);
     var newBooksList = booksList.livros.filter(function(item) {
@@ -68,7 +110,11 @@ app.post('/books/remove', function(request, response) {
     booksList.livros = newBooksList;
 
     fs.writeFile(jsonUrl, JSON.stringify(booksList), function(error) {
-      if(error) return response.send('Not writing possibility');
+      if(error) {
+        console.log('[BOOKS REMOVE WRITE] Error - '+error);
+        response.writeHead(404, {'Content-Type': 'text/html'});
+        return response.end('An inexpected error occurred.');
+      }
       console.log('The books list has been updated');
       response.end();
     });
